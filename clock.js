bcae1d213f88d269b3fd44732f791085d4c961ce
Kristan R <spectre32787@gmail.com>
2021-07-29 12:47:00 -0400

    Got time to print on screen programatically

---
+++ clock.js
@@ -0,0 +1,6 @@
+const time = new Date();
+
+document.getElementById("hours").innerHTML = time.getHours();
+document.getElementById("minutes").innerHTML = time.getMinutes();
+document.getElementById("seconds").innerHTML = time.getSeconds();
+
