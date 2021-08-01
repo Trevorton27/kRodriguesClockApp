bae2815ceddb647e9b52af64d25d0a07a03ee513
Kristan R <spectre32787@gmail.com>
2021-08-01 19:33:13 -0400

    Working on the Hours segment

--- clock.js
+++ clock.js
@@ -1,7 +1,6 @@
 const date = new Date();
-const hourformat = new Array ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]
-const dayOfWeek = new Array ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
+const hourformat = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]
+const dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 let day = date.getDay()
 let month = date.getMonth()
@@ -21,8 +20,9 @@ if (minutes < 10) {
     minutes = "0" + date.getMinutes();  
 }
 
-// if (hours < 12) {
-//     hours = "0" + date.getHours();  
-// }
+if (hours > 12) {
+    hours = hours - 12;
+    hours = "0" + hours;
+}
 
 document.getElementById("time").textContent = `${hours} : ${minutes} : ${seconds}`
