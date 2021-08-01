5ca776e2d812f76df77aaa93bc163da0f577c54c
Kristan R <spectre32787@gmail.com>
2021-08-01 16:49:06 -0400

    Doubel Digits added to time

--- clock.js
+++ clock.js
@@ -1,6 +1,25 @@
-const time = new Date();
+const date = new Date();
 
-document.getElementById("hours").innerHTML = time.getHours();
-document.getElementById("minutes").innerHTML = time.getMinutes();
-document.getElementById("seconds").innerHTML = time.getSeconds();
+let day = date.getDay()
+let month = date.getMonth()
+let year = date.getFullYear()
 
+document.getElementById("date").textContent = `${month} / ${day} / ${year}`
+
+let hours = date.getHours()
+let minutes = date.getMinutes()
+let seconds = date.getSeconds()
+
+if (seconds < 10) {
+    seconds = "0" + date.getSeconds();  
+}
+
+if (minutes < 10) {
+    minutes = "0" + date.getMinutes();  
+}
+
+// if (hours < 10) {
+//     hours = "0" + date.getHours();  
+// }
+
+document.getElementById("time").textContent = `${hours} : ${minutes} : ${seconds}`
