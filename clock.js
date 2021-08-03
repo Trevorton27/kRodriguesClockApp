819e3846faabe6bcf410d557ad95bdc7b60a8aa3
Kristan R <spectre32787@gmail.com>
2021-08-03 11:04:12 -0400

    Completed application and Cleaned up code

--- clock.js
+++ clock.js
@@ -1,43 +1,41 @@
-// Constant Variable Sets
+function clockTick() {
+    const date = new Date();
+    const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-const date = new Date();
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+ 
+    let day = date.getDay()
+    let dayName = dayOfWeek[day];
+    let month = date.getMonth()
+    let monthName = monthOfYear[month];
+    let year = date.getFullYear()
+    
+    document.getElementById("date").textContent = `${dayName}, ${monthName} ${day}, ${year}`;
 
-const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-
-// Section for the Date Block
-
-let day = date.getDay()
-
-let month = date.getMonth()
-let monthName = monthOfYear[month];
-
-let year = date.getFullYear()
-
-document.getElementById("date").textContent = `${monthName} ${day}, ${year}`
-
-// Section for the Time Block
-
-let hours = date.getHours()
-let minutes = date.getMinutes()
-let seconds = date.getSeconds()
-
-document.getElementById("time").textContent = `${hours} : ${minutes} : ${seconds}`;
-
-if (seconds < 10) {
-    seconds = `${"0" + seconds}`;  
-}
-
-if (minutes < 10) {
-    minutes = `${"0" + minutes}`;  
-}
-
-if (hours > 12) {
-    hours = hours - 12;
-    hours = `${"0" + hours}`;
-
-    if (hours === 00) {
-        hours = 12;
+    if (seconds < 10) {
+        seconds = `${"0" + seconds}`;  
     }
+    
+    if (minutes < 10) {
+        minutes = `${"0" + minutes}`;  
+    }
+    
+    if (hours > 12) {
+        hours = hours - 12;
+        hours = `${"0" + hours}`;
+    
+        if (hours === 00) {
+            hours = 12;
+        }
+    }
+    
+    document.getElementById("time").textContent = `${hours} : ${minutes} : ${seconds}`;
 }
+
+setInterval(clockTick, 1000);
+
+
 
