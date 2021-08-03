fbfe54da6ab963d3104db1eb83f5d56763b08db8
Kristan R <spectre32787@gmail.com>
2021-08-03 08:53:01 -0400

    Trying to fix leading zeros

--- clock.js
+++ clock.js
@@ -1,17 +1,16 @@
 // Constant Variable Sets
 
 const date = new Date();
-const dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 const monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
 // Section for the Date Block
 
 let day = date.getDay()
-let dayName = dayOfWeek[date.getDay()];
 
 let month = date.getMonth()
-let monthName = monthOfYear[date.getMonth()];
+let monthName = monthOfYear[month];
 
 let year = date.getFullYear()
 
@@ -26,18 +25,18 @@ let seconds = date.getSeconds()
 document.getElementById("time").textContent = `${hours} : ${minutes} : ${seconds}`;
 
 if (seconds < 10) {
-    seconds = "0" + date.getSeconds();  
+    seconds = `${"0" + seconds}`;  
 }
 
 if (minutes < 10) {
-    minutes = "0" + date.getMinutes();  
+    minutes = `${"0" + minutes}`;  
 }
 
 if (hours > 12) {
     hours = hours - 12;
-    hours = "0" + hours; // hours is now the result of minus 12 for 12H format with a leading 0 added
+    hours = `${"0" + hours}`;
 
-    if (hours === 00 ) {
+    if (hours === 00) {
         hours = 12;
     }
 }
